3c56b13e8122a7578a0b38ba8f70b27d3897d400
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-06-03 15:26:18 +0200

    refactor details overlay

--- js/gallery.js
+++ js/gallery.js
@@ -4,16 +4,19 @@ imageWrappers.forEach((wrapper) => {
   const image = wrapper.querySelector(".photo-frame img");
   const overlay = wrapper.querySelector(".overlay");
   const moreInfo = wrapper.querySelector(".more-info");
+  const details= wrapper.querySelector(".details");
 
   wrapper.addEventListener("mouseover", () => {
     overlay.classList.add("active");
     image.classList.add("active");
     moreInfo.classList.add("active");
+    details.classList.add("active");
 });
 
 wrapper.addEventListener("mouseout", () => {
     overlay.classList.remove("active");
     image.classList.remove("active");
     moreInfo.classList.remove("active");
+    details.classList.remove("active");
   });
 });
